4213d5b7c2cca6a6fa8831797d37efcbd820c475
Dominik Manowski <manowski@protonmail.com>
2018-09-12 16:11:21 +0200

    Add teacher section

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+const teachersBoxes = document.querySelectorAll(".teacher-box");
+const teacherNamesBack = document.querySelectorAll(".teacher-box__name--back")
+
+const url = "https://randomuser.me/api/?results=6&nat=gb,dk,fr&inc=name,picture";
+
+function getTeachers() {
+    fetch(url)
+        .then(res => res.json())
+        .then(teachers => {
+            teachersBoxes.forEach((teacherBox, i) => {
+                teacherBox.children[0].src = teachers.results[i].picture.large;
+                teacherBox.children[1].textContent = `${teachers.results[i].name.first} ${teachers.results[i].name.last}`;
+            })
+            return teachers
+        })
+        .then(teachers => teacherNamesBack.forEach((teacherName, i) => teacherName.textContent = `${teachers.results[i].name.first} ${teachers.results[i].name.last}`))
+        .catch(err => console.log(err))
+}
+
+getTeachers();  
